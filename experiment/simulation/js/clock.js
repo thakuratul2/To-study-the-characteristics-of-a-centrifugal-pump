16f5c391bea3bcc200e8eb106096a08534630598
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-02 17:37:31 +0530

    Update clock

--- experiment/simulation/js/clock.js
+++ experiment/simulation/js/clock.js
@@ -1,22 +1,54 @@
-const secondHand = document.querySelector('.second-hand');
+// const secondHand = document.querySelector('.second-hand');
 
 
  
- function setDate() {
-  const now = new Date();
+//  function setDate() {
+//   const now = new Date();
  
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+//   const seconds = now.getSeconds();
+//   const secondsDegrees = ((seconds / 60) * 360) + 90;
+//   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
  
   
-}
+// }
  
-setInterval(setDate, 1000);
-setTimeout(stopTime,30000);
+// setInterval(setDate, 1000);
+// setTimeout(stopTime,30000);
 
-function stopTime(){
-    clearInterval(setDate);
-}
+// function stopTime(){
+//     clearInterval(setDate);
+// }
  
-setDate();
+
+
+// setDate();
+
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
