e9a845cee40d06e53b9fafef055791b750e882ff
thakuratul2 <pratapsinghatul111@gmail.com>
2023-07-12 20:44:25 +0530

    Simulationn 4

--- experiment/simulation/js/valve.js
+++
@@ -1,9 +0,0 @@
-const light = document.getElementById('led-yellow');
-const btn = document.getElementsByClassName('valve');
-
-
-btn.addEventListener('click', function() {
-  // Toggle the class of the light element
-  light.classList.toggle('led-yellow');
-  light.classList.toggle('off');
-});
